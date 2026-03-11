14f150766d89afc177c38921bab5a625cd208985
kHaNyA85 <4531795@myuwc.ac.za>
2026-03-11 14:55:34 +0200

    Update Anagrams.java by removing errors

--- Anagrams.java
+++ Anagrams.java
@@ -1,55 +1,62 @@
 import java.io.*;
 import java.util.*;
 
-public class Anagrams{
+public class Anagrams {
+
   public static String signature(String word) {
     char[] letters = word.toCharArray();
-    Array.sort(letters);
+    Arrays.sort(letters);
     return new String(letters);
+  }
 
-    }
   public static void main(String[] args) {
-    HashMap < String, ArrayList < String >> D = new HashMap<>();
 
-    try{
+    HashMap<String, ArrayList<String>> D = new HashMap<>();
+
+    try {
       BufferedReader br = new BufferedReader(new FileReader("joyce1922_ulysses-1.text"));
-      String Line;
+      String line;
 
       while ((line = br.readLine()) != null) {
+
         String[] words = line.split("\\s+");
 
         for (String w : words) {
-          w = w.replaceAll("[.,;,:_!-]");
+
+          w = w.replaceAll("[.,;:_!\\-]", "");
           w = w.toLowerCase();
 
           if (w.length() == 0) {
             continue;
           }
+
           String key = signature(w);
 
           if (!D.containsKey(key)) {
-            ArrayList < String > list = new ArrayList<>();
+            ArrayList<String> list = new ArrayList<>();
             list.add(w);
             D.put(key, list);
-          }
+          } 
           else {
             D.get(key).add(w);
           }
-          
+
         }
       }
+
       br.close();
+
       for (String key : D.keySet()) {
-        ArrayList < String > list = D.get(key);
+        ArrayList<String> list = D.get(key);
 
         if (list.size() >= 2) {
-          System.out.println("key + ":" + list);
+          System.out.println(key + ": " + list);
         }
       }
-    }
-    catch(IOException e) {
+
+    } 
+    catch (IOException e) {
       System.out.println("Error reading file.");
     }
   }
-  
 }
